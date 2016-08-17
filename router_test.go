1dea614ac312b3bb370ad4b723d1eef7e3a220fc
nirasan <nirasan@gmail.com>
2016-08-17 14:01:02 +0900

    add router test

---
+++ router_test.go
@@ -0,0 +1,60 @@
+package golem
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+type HelloRequest struct {
+	Msg string `json:"msg"`
+}
+
+type HelloResponse struct {
+	Msg string `json:"msg"`
+}
+
+func Hello(conn *Connection, data *HelloRequest) {
+	conn.Emit("hello", &HelloResponse{Msg: "hello " + data.Msg})
+}
+
+func TestRouter_Handler(t *testing.T) {
+	myrouter := NewRouter()
+	myrouter.On("hello", Hello)
+	ts := httptest.NewServer(http.HandlerFunc(myrouter.Handler()))
+	defer ts.Close()
+
+	dialer := websocket.Dialer{
+		Subprotocols:    []string{},
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+
+	url := strings.Replace(ts.URL, "http://", "ws://", 1)
+	header := http.Header{"Accept-Encoding": []string{"gzip"}}
+
+	conn, _, err := dialer.Dial(url, header)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+
+	err = conn.WriteMessage(websocket.TextMessage, []byte("hello {\"msg\":\"my message\"}"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	messageType, p, err := conn.ReadMessage()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if messageType != websocket.TextMessage {
+		t.Error("invalid message type")
+	}
+	if string(p) != "hello {\"msg\":\"hello my message\"}" {
+		t.Error(fmt.Sprintf("invalid message: %s", string(p)))
+	}
+}
