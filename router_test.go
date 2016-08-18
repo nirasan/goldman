c81fda19e537f6b099df43d2de1f568e63dc58c2
nirasan <nirasan@gmail.com>
2016-08-18 09:01:51 +0900

    add Router.On test

--- router_test.go
+++ router_test.go
@@ -1,11 +1,13 @@
 package golem
 
 import (
+	"errors"
 	"github.com/gorilla/websocket"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 )
 
 type HelloRequest struct {
@@ -21,21 +23,92 @@ func Hello(conn *Connection, data *HelloRequest) {
 }
 
 func TestRouter_Handler(t *testing.T) {
-	myrouter := NewRouter()
-	myrouter.On("hello", Hello)
+	router := NewRouter()
+	router.On("hello", Hello)
 
-	ts, conn := createServerClient(t, myrouter)
+	ts, conn, err := createServerClient(router)
+	fatalNotNil(t, err)
+
 	defer ts.Close()
 	defer conn.Close()
 
-	writeMessage(t, conn, `hello {"name":"goldman"}`)
-	res := readMessage(t, conn)
+	err = writeMessage(conn, `hello {"name":"goldman"}`)
+	fatalNotNil(t, err)
+
+	res, err := readMessage(conn)
+	fatalNotNil(t, err)
 	if res != `hello {"msg":"hello goldman"}` {
 		t.Error("invalid message: " + res)
 	}
 }
 
-func createServerClient(t *testing.T, router *Router) (*httptest.Server, *websocket.Conn) {
+type RequestPlus struct {
+	Num1 int `json:"num1"`
+	Num2 int `json:"num2"`
+}
+
+type ResponsePlus struct {
+	Num int `json:"num"`
+}
+
+type RequestMinus struct {
+	Num1 int `json:"num1"`
+	Num2 int `json:"num2"`
+}
+
+type ResponseMinus struct {
+	Num int `json:"num"`
+}
+
+func Plus(conn *Connection, data *RequestPlus) {
+	conn.Emit("plus", &ResponsePlus{Num: data.Num1 + data.Num2})
+}
+
+func Minus(conn *Connection, data *RequestMinus) {
+	conn.Emit("minus", &ResponseMinus{Num: data.Num1 - data.Num2})
+}
+
+func TestRouter_On(t *testing.T) {
+	router := NewRouter()
+	router.On("plus", Plus)
+	router.On("minus", Minus)
+
+	ts, conn, err := createServerClient(router)
+	fatalNotNil(t, err)
+
+	defer ts.Close()
+	defer conn.Close()
+
+	err = writeMessage(conn, `plus {"num1":1,"num2":2}`)
+	fatalNotNil(t, err)
+
+	res, err := readMessage(conn)
+	fatalNotNil(t, err)
+
+	if res != `plus {"num":3}` {
+		t.Error("invalid message: " + res)
+	}
+
+	err = writeMessage(conn, `minus {"num1":1,"num2":2}`)
+	fatalNotNil(t, err)
+
+	res, err = readMessage(conn)
+	fatalNotNil(t, err)
+
+	if res != `minus {"num":-1}` {
+		t.Error("invalid message: " + res)
+	}
+
+	err = writeMessage(conn, `hello {"name":"goldman"}`)
+	fatalNotNil(t, err)
+
+	_, err = readMessage(conn)
+	if err == nil {
+		t.Error("not registered callback executed")
+	}
+}
+
+func createServerClient(router *Router) (*httptest.Server, *websocket.Conn, error) {
 	ts := httptest.NewServer(http.HandlerFunc(router.Handler()))
 
 	dialer := websocket.Dialer{
@@ -49,26 +122,34 @@ func createServerClient(t *testing.T, router *Router) (*httptest.Server, *websoc
 
 	conn, _, err := dialer.Dial(url, header)
 	if err != nil {
-		t.Fatal(err)
+		return nil, nil, err
 	}
 
-	return ts, conn
+	return ts, conn, nil
 }
 
-func writeMessage(t *testing.T, conn *websocket.Conn, message string) {
+func writeMessage(conn *websocket.Conn, message string) error {
 	err := conn.WriteMessage(websocket.TextMessage, []byte(message))
 	if err != nil {
-		t.Fatal(err)
+		return err
 	}
+	return nil
 }
 
-func readMessage(t *testing.T, conn *websocket.Conn) string {
+func readMessage(conn *websocket.Conn) (string, error) {
+	conn.SetReadDeadline(time.Now().Add(500000000 * time.Nanosecond))
 	messageType, p, err := conn.ReadMessage()
 	if err != nil {
-		t.Fatal(err)
+		return "", err
 	}
 	if messageType != websocket.TextMessage {
-		t.Error("invalid message type")
+		return "", errors.New("invalid message type")
 	}
-	return string(p)
+	return string(p), nil
+}
+
+func fatalNotNil(t *testing.T, e error) {
+	if e != nil {
+		t.Fatal(e)
+	}
 }
